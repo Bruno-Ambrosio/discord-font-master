8a5b48394bba37b811750954cb96a1c625d0075b
Bruno <brunoalvesambrosio@gmail.com>
2023-08-14 01:42:53 -0300

    discord commands - auto registro de comandos

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits, Collection, PermissionsBitField } = require('discord.js');
+const { REST, Routes } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const { TOKEN } = process.env;
 const fs = require('node:fs');
 const path = require('node:path');
 const trocarfonte = require('./commands/trocarfonte');
@@ -40,7 +41,7 @@ client.login(TOKEN);
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) {
         return;
-    } else if(interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)){
+    } else if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
         const command = interaction.client.commands.get(interaction.commandName);
         if (!command) {
             console.error('comando não encontrado!')
@@ -61,9 +62,34 @@ client.on(Events.GuildMemberAdd, async member => {
             options: { getInteger: () => number },
             reply: async (message) => { console.log(message); }
         };
-        
+
         await trocarfonte.execute(fakeInteraction);
     } catch (error) {
         console.error(error);
     }
+});
+
+client.on(Events.GuildCreate, guild => {
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const { TOKEN, CLIENT_ID } = process.env;
+    const commands = [];
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
+    const rest = new REST({version: '10'}).setToken(TOKEN);
+    (async () => {
+        try {
+            console.log(`Resetando ${commands.length} comandos`);
+            const data = await rest.put(
+                Routes.applicationGuildCommands(CLIENT_ID, guild.id, guild.id),
+                { body: commands }
+            );
+            console.log('comando(s) registrado(s) com sucesso!')
+        } catch (er) {
+            console.log(er);
+        }
+    })()
 });
